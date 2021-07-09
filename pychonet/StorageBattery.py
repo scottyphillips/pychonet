7f4283da22a16763d4f499e5aaafc1b0d9cd663d
gvswastaken <2411463+gvswastaken@users.noreply.github.com>
2021-07-09 16:44:46 +0900

    Export battery states to consumers, simplify return values.

--- pychonet/StorageBattery.py
+++ pychonet/StorageBattery.py
@@ -1,10 +1,9 @@
 from pychonet.EchonetInstance import EchonetInstance
 
-def _027de4(edt):
-    return {'remaining_electricity_3': int.from_bytes(edt, 'big')}
+class StorageBattery(EchonetInstance):
 
-def _027dcf(edt):
-    STATES = {
+    WORKING_OPERATION_STATES = {
+        0x40: "Other",
         0x41: "Rapid charging",
         0x42: "Charging",
         0x43: "Discharging",
@@ -15,16 +14,13 @@ def _027dcf(edt):
         0x49: "Effective capacity recalculation processing"
     }
 
-    return {"working_operation_status": STATES[int.from_bytes(edt, 'big')]}
-
-class StorageBattery(EchonetInstance):
     def __init__(self, netif, instance = 0x1):
         self.eojgc = 0x02
         self.eojcc = 0x7d
         EchonetInstance.__init__(self, self.eojgc, self.eojcc, instance, netif)
 
     def getRemainingStoredElectricity3(self):
-        return _027de4(self.getSingleMessageResponse(0xE4))
+        return int.from_bytes(self.getSingleMessageResponse(0xE4), 'big')
 
     def getWorkingOperationStatus(self):
-        return _027dcf(self.getSingleMessageResponse(0xCF))
+        return int.from_bytes(self.getSingleMessageResponse(0xCF), 'big')
