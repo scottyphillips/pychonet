febf8adc5ed2c72aad26707b8cbf04db6d200f79
Jason Nader <ammgws@users.noreply.github.com>
2021-09-13 22:15:32 +0900

    Fix class variable instantiation

--- pychonet/HomeSolarPower.py
+++ pychonet/HomeSolarPower.py
@@ -2,8 +2,8 @@ from pychonet.EchonetInstance import EchonetInstance
 
 class HomeSolarPower(EchonetInstance):
     def __init__(self, host, api_connector, instance = 0x1):
-        self.eojgc = 0x02
-        self.eojcc = 0x79
+        self._eojgc = 0x02
+        self._eojcc = 0x79
         EchonetInstance.__init__(self, host, self._eojgc, self._eojcc, instance, api_connector)
 
     def getMeasuredInstantPower(self):
