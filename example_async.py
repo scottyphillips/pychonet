3a35e54bf20184689baa28749e56e024e0683fde
Scott Phillips <scotty.phillips@hotmail.com>
2021-09-15 14:06:49 +1000

    fix to async file

--- example_async.py
+++ example_async.py
@@ -12,6 +12,7 @@ async def main():
     loop = asyncio.get_event_loop()
     udp.run("0.0.0.0", 3610, loop=loop)
     server = api(server=udp, loop=loop)
+    server._message_timeout = 300
 
     host = '192.168.1.6'
     await server.discover(host)
