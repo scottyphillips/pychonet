6df044934984601e6b9db278ccc7dc7bc525fedb
Scott Phillips <scotty.phillips@hotmail.com>
2021-09-18 17:04:41 +1000

    Fix for issue #27

--- pychonet/__init__.py
+++ pychonet/__init__.py
@@ -10,8 +10,10 @@ from .GeneralLighting import GeneralLighting
 from pychonet.lib.eojx import EOJX_CLASS
 
 def Factory(host, server, eojgc, eojcc, eojci= 0x01):
-
-    instance = EOJX_CLASS[eojgc][eojcc]
+    Instance = None
+    if eojgc in EOJX_CLASS:
+        if eojcc in  EOJX_CLASS[eojgc]:
+            instance = EOJX_CLASS[eojgc][eojcc]
 
     """Factory Method"""
     # TODO - probably a much cleaner way of doing this.
