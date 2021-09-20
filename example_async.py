7aeaf848f5abdb1d78cd836725d04a15de40c564
Jason Nader <jason.nader@protonmail.com>
2021-09-20 21:48:00 +0900

    Fix formatting, improve readability

--- example_async.py
+++ example_async.py
@@ -16,35 +16,46 @@ async def main():
     server._debug_flag = False
     server._message_timeout = 300
 
-    host = '192.168.1.6'
+    host = "192.168.1.6"
     await server.discover(host)
     # Timeout after 3 seconds
     for x in range(0, 300):
         await asyncio.sleep(0.01)
-        if 'discovered' in list(server._state[host]):
-             print(f"{host} - ECHONET Node Discovery Successful!")
-             break
+        if "discovered" in list(server._state[host]):
+            print(f"{host} - ECHONET Node Discovery Successful!")
+            break
 
     instance_list = []
     state = server._state[host]
-    for eojgc in list(state['instances'].keys()):
-        for eojcc in list(state['instances'][eojgc].keys()):
-            for instance in list(state['instances'][eojgc][eojcc].keys()):
-                  instance_info = f"{hex(eojgc)}-{hex(eojcc)}-{hex(instance)}"
-                  await server.getAllPropertyMaps(host, eojgc, eojcc, 0x00)  # issue here??
-                  print(f"{host} - ECHONET Instance {instance_info} map attributes discovered!")
-                  print(f"get map is - {state['instances'][eojgc][eojcc][instance][ENL_GETMAP]}")
-                  print(f"set map is - {state['instances'][eojgc][eojcc][instance][ENL_SETMAP]}")
-                  getmap = [hex(e) for e in state['instances'][eojgc][eojcc][instance][ENL_GETMAP]]
-                  setmap = [hex(e) for e in state['instances'][eojgc][eojcc][instance][ENL_SETMAP]]
+    for eojgc in list(state["instances"].keys()):
+        for eojcc in list(state["instances"][eojgc].keys()):
+            for instance in list(state["instances"][eojgc][eojcc].keys()):
+                instance_info = f"{hex(eojgc)}-{hex(eojcc)}-{hex(instance)}"
+                await server.getAllPropertyMaps(host, eojgc, eojcc, 0x00)  # issue here??
+                print(f"{host} - ECHONET Instance {instance_info} map attributes discovered!")
+                print(f"get map is - {state['instances'][eojgc][eojcc][instance][ENL_GETMAP]}")
+                print(f"set map is - {state['instances'][eojgc][eojcc][instance][ENL_SETMAP]}")
 
-                  await server.getIdentificationInformation(host, eojgc, eojcc, instance)
-                  uid = state['instances'][eojgc][eojcc][instance][ENL_UID]
-                  manufacturer = state['instances'][eojgc][eojcc][instance][ENL_MANUFACTURER]
-                  print(f"{host} - ECHONET Instance {instance_info} identification number discovered!")
-                  instance_list.append({"host":host,"eojgc":hex(eojgc),"eojcc":hex(eojcc),"eojci":hex(instance),"getmap":getmap,"setmap":setmap,"uid":uid,"manufacturer":manufacturer})
+                await server.getIdentificationInformation(host, eojgc, eojcc, instance)
+                uid = state["instances"][eojgc][eojcc][instance][ENL_UID]
+                manufacturer = state["instances"][eojgc][eojcc][instance][
+                    ENL_MANUFACTURER
+                ]
+                print(f"{host} - ECHONET Instance {instance_info} identification number discovered!")
+                instance_list.append(
+                    {
+                        "host": host,
+                        "eojgc": hex(eojgc),
+                        "eojcc": hex(eojcc),
+                        "eojci": hex(instance),
+                        "getmap": [hex(e) for e in state["instances"][eojgc][eojcc][instance][ENL_GETMAP]],
+                        "setmap": [hex(e) for e in state["instances"][eojgc][eojcc][instance][ENL_SETMAP]],
+                        "uid": uid,
+                        "manufacturer": manufacturer,
+                    }
+                )
 
         pprint(instance_list)
-#    print(server._message_list)
+
 if __name__ == "__main__":
     asyncio.run(main())
