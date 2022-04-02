83a101061d1e14069a96b0d952e4c0c1f236e4c7
scottyphillips <s3288103@student.rmit.edu.au>
2022-04-02 15:09:06 +1100

    Version 2.2.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, "README.md")) as f:
 
 setup(
     name="pychonet",
-    version="2.2.0",
+    version="2.2.1",
     author="Scott Phillips",
     author_email="scotty.phillips@hotmail.com",
     packages=["pychonet", "pychonet.lib"],
