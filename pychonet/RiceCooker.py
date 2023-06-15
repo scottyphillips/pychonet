5b5ae89f7f7af19372705ad2e2e9ab1d75e72c86
Scott Phillips <scotty.phillips@hotmail.com>
2023-06-15 20:37:07 +1000

    Update RiceCooker.py

--- pychonet/RiceCooker.py
+++ pychonet/RiceCooker.py
@@ -1,10 +1,79 @@
-from pychonet import EchonetInstance
+from pychonet.EchonetInstance import EchonetInstance
 from pychonet.lib.epc_functions import _hh_mm
+
+@staticmethod
+def _03BB80(payload):
+    return 'On' if payload == 0x30 else 'Off'
+
+@staticmethod
+def _03BBB0(payload):
+    return 'Open' if payload == 0x30 else 'Closed'
+
+@staticmethod
+def _03BBB1(payload):
+    if payload == 0x41:
+        return 'Rice cooking completed'
+    elif payload == 0x42:
+        return 'Rice cooking in progress'
+    elif payload == 0x43:
+        return 'Rice cooking paused'
+    elif payload == 0x44:
+        return 'Rice cooking aborted'
+    else:
+        return 'Unknown'
+
+@staticmethod
+def _03BBB2(payload):
+    return payload
+
+@staticmethod
+def _03BBE1(payload):
+    if payload == 0x41:
+        return 'Warmer on'
+    elif payload == 0x42:
+        return 'Warmer off'
+    else:
+        return 'Unknown warmer setting'
+
+@staticmethod
+def _03BBE5(payload):
+    if payload == 0x41:
+        return 'Inner pot installed'
+    elif payload == 0x42:
+        return 'Inner pot removed'
+    else:
+        return 'Unknown inner pot removal status'
+
+@staticmethod
+def _03BBE6(payload):
+    if payload == 0x41:
+        return 'Cover installed'
+    elif payload == 0x42:
+        return 'Cover removed'
+    else:
+        return 'Unknown cover removal status'
+
+@staticmethod
+def _03BB90(payload):
+    return payload
 
 class RiceCooker(EchonetInstance):
     class_codes = {
         'class_group_code': 0x03,
         'class_code': 0xBB
+    }
+
+    EPC_FUNCTIONS = {
+        0x80: _03BB80,
+        0xB0: _03BBB0,
+        0xB1: _03BBB1,
+        0xB2: _03BBB2,
+        0xE1: _03BBE1,
+        0xE5: _03BBE5,
+        0xE6: _03BBE6,
+        0x90: _03BB90,
+        0x91: _hh_mm,
+        0x92: _hh_mm,
     }
 
     def __init__(self, host, api_connector=None, instance=0x1):
@@ -60,71 +129,3 @@ class RiceCooker(EchonetInstance):
     def setRiceCookingReservationSettingRelativeTime(self, time):
         self.setMessage(0x92, time)
 
-    @staticmethod
-    def _03BB80(payload):
-        return 'On' if payload == 0x30 else 'Off'
-
-    @staticmethod
-    def _03BBB0(payload):
-        return 'Open' if payload == 0x30 else 'Closed'
-
-    @staticmethod
-    def _03BBB1(payload):
-        if payload == 0x41:
-            return 'Rice cooking completed'
-        elif payload == 0x42:
-            return 'Rice cooking in progress'
-        elif payload == 0x43:
-            return 'Rice cooking paused'
-        elif payload == 0x44:
-            return 'Rice cooking aborted'
-        else:
-            return 'Unknown'
-
-    @staticmethod
-    def _03BBB2(payload):
-        return payload
-
-    @staticmethod
-    def _03BBE1(payload):
-        if payload == 0x41:
-            return 'Warmer on'
-        elif payload == 0x42:
-            return 'Warmer off'
-        else:
-            return 'Unknown warmer setting'
-
-    @staticmethod
-    def _03BBE5(payload):
-        if payload == 0x41:
-            return 'Inner pot installed'
-        elif payload == 0x42:
-            return 'Inner pot removed'
-        else:
-            return 'Unknown inner pot removal status'
-
-    @staticmethod
-    def _03BBE6(payload):
-        if payload == 0x41:
-            return 'Cover installed'
-        elif payload == 0x42:
-            return 'Cover removed'
-        else:
-            return 'Unknown cover removal status'
-
-    @staticmethod
-    def _03BB90(payload):
-        return payload
-
-    EPC_FUNCTIONS = {
-        0x80: _03BB80,
-        0xB0: _03BBB0,
-        0xB1: _03BBB1,
-        0xB2: _03BBB2,
-        0xE1: _03BBE1,
-        0xE5: _03BBE5,
-        0xE6: _03BBE6,
-        0x90: _03BB90,
-        0x91: _hh_mm,
-        0x92: _hh_mm,
-    }
