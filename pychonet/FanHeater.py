bf840615c75d2907eb0d250cdfc513d80b192861
Scott Phillips <scotty.phillips@hotmail.com>
2023-06-15 20:32:07 +1000

    ChatGPT Generated Classes!

---
+++ pychonet/FanHeater.py
@@ -0,0 +1,147 @@
+from pychonet import EchonetInstance
+from pychonet.lib.epc_functions import _hh_mm, _int
+
+class FanHeater(EchonetInstance):
+    class_codes = {
+        'class_group_code': 0x01,
+        'class_code': 0x43
+    }
+
+    EPC_FUNCTIONS = {
+        0xB3: _int,
+        0xBB: _0143BB,
+        0xB1: _0143B1,
+        0x90: _014390,
+        0x91: _hh_mm,
+        0x92: _hh_mm,
+        0x94: _014394,
+        0x95: _hh_mm,
+        0x96: _hh_mm,
+        0xC0: _0143C0,
+        0xC1: _hh_mm,
+        0xC2: _0143C2,
+        0xC3: _0143C3,
+        0xC4: _0143C4,
+    }
+
+    def __init__(self, host, api_connector=None, instance=0x1):
+        self._eojgc = self.class_codes['class_group_code']
+        self._eojcc = self.class_codes['class_code']
+        super().__init__(host, self._eojgc, self._eojcc, instance, api_connector)
+
+    def getTemperatureSettingValue(self):
+        return self.getMessage(0xB3)
+
+    def setTemperatureSettingValue(self, value):
+        self.setMessage(0xB3, value)
+
+    def getMeasuredTemperature(self):
+        return self.getMessage(0xBB)
+
+    def getAutomaticTemperatureControlSetting(self):
+        return self.getMessage(0xB1)
+
+    def setAutomaticTemperatureControlSetting(self, setting):
+        self.setMessage(0xB1, setting)
+
+    def getOnTimerReservationSetting(self):
+        return self.getMessage(0x90)
+
+    def setOnTimerReservationSetting(self, setting):
+        self.setMessage(0x90, setting)
+
+    def getOnTimerSettingValue(self):
+        return self.getMessage(0x91)
+
+    def setOnTimerSettingValue(self, value):
+        self.setMessage(0x91, value)
+
+    def getOnTimerSettingRelativeTime(self):
+        return self.getMessage(0x92)
+
+    def setOnTimerSettingRelativeTime(self, value):
+        self.setMessage(0x92, value)
+
+    def getOffTimerReservationSetting(self):
+        return self.getMessage(0x94)
+
+    def setOffTimerReservationSetting(self, setting):
+        self.setMessage(0x94, setting)
+
+    def getOffTimerSettingValue(self):
+        return self.getMessage(0x95)
+
+    def setOffTimerSettingValue(self, value):
+        self.setMessage(0x95, value)
+
+    def getOffTimerSettingRelativeTime(self):
+        return self.getMessage(0x96)
+
+    def setOffTimerSettingRelativeTime(self, value):
+        self.setMessage(0x96, value)
+
+    def getExtensionalOperationSetting(self):
+        return self.getMessage(0xC0)
+
+    def setExtensionalOperationSetting(self, setting):
+        self.setMessage(0xC0, setting)
+
+    def getExtensionalOperationTimerTimeSettingValue(self):
+        return self.getMessage(0xC1)
+
+    def setExtensionalOperationTimerTimeSettingValue(self, value):
+        self.setMessage(0xC1, value)
+
+    def getIonEmissionSetting(self):
+        return self.getMessage(0xC2)
+
+    def setIonEmissionSetting(self, setting):
+        self.setMessage(0xC2, setting)
+
+    def getImplementedIonEmissionMethod(self):
+        return self.getMessage(0xC3)
+
+    def getOilAmountLevel(self):
+        return self.getMessage(0xC4)
+
+    @staticmethod
+    def _0143BB(payload):
+        return str(payload)
+
+    @staticmethod
+    def _0143B1(payload):
+        return str(payload)
+
+    @staticmethod
+    def _014390(payload):
+        return str(payload)
+
+    @staticmethod
+    def _014394(payload):
+        return str(payload)
+
+    @staticmethod
+    def _0143C0(payload):
+        if payload == 0x41:
+            return 'Extension ON'
+        elif payload == 0x42:
+            return 'Extension OFF'
+        else:
+            return 'Unknown'
+
+    @staticmethod
+    def _0143C2(payload):
+        if payload == 0x41:
+            return 'Ion emission ON'
+        elif payload == 0x42:
+            return 'Ion emission OFF'
+        else:
+            return 'Unknown'
+
+    @staticmethod
+    def _0143C3(payload):
+        return str(payload)
+
+    @staticmethod
+    def _0143C4(payload):
+        return str(payload)
---
+++ pychonet/RiceCooker.py
@@ -0,0 +1,130 @@
+from pychonet import EchonetInstance
+from pychonet.lib.epc_functions import _hh_mm
+
+class RiceCooker(EchonetInstance):
+    class_codes = {
+        'class_group_code': 0x03,
+        'class_code': 0xBB
+    }
+
+    def __init__(self, host, api_connector=None, instance=0x1):
+        self._eojgc = self.class_codes['class_group_code']
+        self._eojcc = self.class_codes['class_code']
+        super().__init__(host, self._eojgc, self._eojcc, instance, api_connector)
+
+    def getOperationStatus(self):
+        return self.getMessage(0x80)
+
+    def setOperationStatus(self, status):
+        self.setMessage(0x80, status)
+
+    def getCoverStatus(self):
+        return self.getMessage(0xB0)
+
+    def getRiceCookingStatus(self):
+        return self.getMessage(0xB1)
+
+    def getRiceCookingControlSetting(self):
+        return self.getMessage(0xB2)
+
+    def setRiceCookingControlSetting(self, setting):
+        self.setMessage(0xB2, setting)
+
+    def getWarmerSetting(self):
+        return self.getMessage(0xE1)
+
+    def setWarmerSetting(self, setting):
+        self.setMessage(0xE1, setting)
+
+    def getInnerPotRemovalStatus(self):
+        return self.getMessage(0xE5)
+
+    def getCoverRemovalStatus(self):
+        return self.getMessage(0xE6)
+
+    def getRiceCookingReservationSetting(self):
+        return self.getMessage(0x90)
+
+    def setRiceCookingReservationSetting(self, setting):
+        self.setMessage(0x90, setting)
+
+    def getRiceCookingReservationSettingTime(self):
+        return self.getMessage(0x91)
+
+    def setRiceCookingReservationSettingTime(self, time):
+        self.setMessage(0x91, time)
+
+    def getRiceCookingReservationSettingRelativeTime(self):
+        return self.getMessage(0x92)
+
+    def setRiceCookingReservationSettingRelativeTime(self, time):
+        self.setMessage(0x92, time)
+
+    @staticmethod
+    def _03BB80(payload):
+        return 'On' if payload == 0x30 else 'Off'
+
+    @staticmethod
+    def _03BBB0(payload):
+        return 'Open' if payload == 0x30 else 'Closed'
+
+    @staticmethod
+    def _03BBB1(payload):
+        if payload == 0x41:
+            return 'Rice cooking completed'
+        elif payload == 0x42:
+            return 'Rice cooking in progress'
+        elif payload == 0x43:
+            return 'Rice cooking paused'
+        elif payload == 0x44:
+            return 'Rice cooking aborted'
+        else:
+            return 'Unknown'
+
+    @staticmethod
+    def _03BBB2(payload):
+        return payload
+
+    @staticmethod
+    def _03BBE1(payload):
+        if payload == 0x41:
+            return 'Warmer on'
+        elif payload == 0x42:
+            return 'Warmer off'
+        else:
+            return 'Unknown warmer setting'
+
+    @staticmethod
+    def _03BBE5(payload):
+        if payload == 0x41:
+            return 'Inner pot installed'
+        elif payload == 0x42:
+            return 'Inner pot removed'
+        else:
+            return 'Unknown inner pot removal status'
+
+    @staticmethod
+    def _03BBE6(payload):
+        if payload == 0x41:
+            return 'Cover installed'
+        elif payload == 0x42:
+            return 'Cover removed'
+        else:
+            return 'Unknown cover removal status'
+
+    @staticmethod
+    def _03BB90(payload):
+        return payload
+
+    EPC_FUNCTIONS = {
+        0x80: _03BB80,
+        0xB0: _03BBB0,
+        0xB1: _03BBB1,
+        0xB2: _03BBB2,
+        0xE1: _03BBE1,
+        0xE5: _03BBE5,
+        0xE6: _03BBE6,
+        0x90: _03BB90,
+        0x91: _hh_mm,
+        0x92: _hh_mm,
+    }
