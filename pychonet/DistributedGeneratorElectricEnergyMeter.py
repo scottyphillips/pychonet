b41a0eff593e868a460b99535f703e655feb2e5a
samqixin <samqixin@gmail.com>
2026-03-09 00:44:08 +0900

    revised bug?

--- pychonet/DistributedGeneratorElectricEnergyMeter.py
+++ pychonet/DistributedGeneratorElectricEnergyMeter.py
@@ -3,29 +3,26 @@ from pychonet.EchonetInstance import EchonetInstance
 from pychonet.lib.epc_functions import (
     _int,
     _signed_int,
-    _to_string,
     _yyyy_mm_dd,
-    _hh_mm,
 )
 
 class DistributedGeneratorElectricEnergyMeter(EchonetInstance):
+    DICT_UNIT_OF_ENERGY = {
+        0x00: "1 kWh",
+        0x01: "0.1 kWh",
+        0x02: "0.01 kWh",
+        0x03: "0.001 kWh",
+        0x04: "0.0001 kWh",
+        0x0A: "10 kWh",
+        0x0B: "100 kWh",
+        0x0C: "1000 kWh",
+        0x0D: "10000 kWh",
+    }
+
     EPC_FUNCTIONS = {
         0x80: _int, # Operation status
         0x98: _yyyy_mm_dd, # Current date setting
-        0xD4: [
-            _int,
-            {
-                0x00: 1,
-                0x01: 0.1,
-                0x02: 0.01,
-                0x03: 0.001,
-                0x04: 0.0001,
-                0x0A: 10,
-                0x0B: 100,
-                0x0C: 1000,
-                0x0D: 10000,
-            },
-        ], # Unit for cumulative amount of electric energy
+        0xD4: [_int,DICT_UNIT_OF_ENERGY], # Unit for cumulative amount of electric energy
         0xE0: _int, # Measured cumulative amount of electric energy (AC input)
         0xE2: _int, # Measured cumulative amount of electric energy (AC output)
         0xE4: _int, # Measured cumulative amount of electric energy (independent output)
